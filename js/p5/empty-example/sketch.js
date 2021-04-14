36fe6c8d79b471f0a8d343231612c4f782ab9e5a
H1rono <hronok66@gmail.com>
2021-04-14 11:25:45 +0000

    Add p5.js, .gitignore

---
+++ js/p5/empty-example/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  // put setup code here
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  // put drawing code here
+	background(255);
+	fill(255, 0, 0);
+	ellipse(100, 100, 100, 100);
+}
